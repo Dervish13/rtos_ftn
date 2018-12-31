e935512cb1d48e7ccdb7aa49a273eadd27fb2787
Rajko Dervisevic <rajkod@hotmail.com>
2018-12-31 02:07:50 +0100

    server minor tweaks

--- tcp/server.c
+++ tcp/server.c
@@ -5,13 +5,25 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+int znak (char c) {
+  if (c == '+' || c == '-' || c == '*' || c == '/') {
+    return 1;
+  }
+  return 0;
+}
 
+
+float racun( char* jednacina)
+{
+
+
+}
 int main() {
   /* char poruka_servera[30] = "Dobrodosli na tcp digitron \n\n"; */
 
   // pravimo socket servera
   int server_socket;
-  char jednacina[20]={0};
+  char jednacina[20] = {0};
   server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
   // adresa servera
@@ -32,9 +44,9 @@ int main() {
   /* client_socket3 = accept(server_socket, NULL, NULL); */
   /* client_socket4 = accept(server_socket, NULL, NULL); */
   /* client_socket5 = accept(server_socket, NULL, NULL); */
-  read(client_socket1,jednacina,sizeof(jednacina));
-  printf("%s",jednacina);
-  float a=33;
+  read(client_socket1, jednacina, sizeof(jednacina));
+  printf("%s", jednacina);
+  float a = 33;
   // posalji klijentu
   send(client_socket1, &a, sizeof(float), 0);
   // zatvori socket
@@ -46,6 +58,6 @@ int main() {
   /* // zatvori socket */
   /* send(client_socket5, poruka_servera, sizeof(poruka_servera), 0); */
   /* // zatvori socket */
-  close (server_socket);
+  close(server_socket);
   return 0;
 }
